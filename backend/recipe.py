3341a3c8a90e64754f4d55ff586fc5fab5d71ee4
etoain-shrdlu <nc8774@gmail.com>
2022-10-15 14:50:35 -0400

    more

--- backend/recipe.py
+++ backend/recipe.py
@@ -1,7 +1,8 @@
 from user import User
+from food import Restriction_Categories, Recipe_Categories
 
 class Recipe:
-    def __init__(self, name : str, description : str, imgs, ingredients, directions, calories, category):
+    def __init__(self, name : str, description : str, imgs, ingredients, directions, calories, category : Recipe_Categories):
         self.name = name
         self.description = description
         self.imgs = imgs
@@ -10,6 +11,6 @@ class Recipe:
         self.calories = calories
         self.category = category
         
-        self.ratings = []
+        self.rating = 5.0
 
         self.tags = []
--- backend/user.py
+++ backend/user.py
@@ -2,31 +2,32 @@ from recipe import Recipe
 from enum import Enum
 from main import Database
 
-from food import Restriction_Categories
+from food import Restriction_Categories, Recipe_Categories
 
 class User:
     def __init__(self, restrictions_strict : list(Restriction_Categories), restrictions_preference : list(Restriction_Categories), **kwargs : ...):
         
         self.restrictions_strict = restrictions_strict
         self.restrictions_preference = restrictions_preference
-
-        self.rankings = ... 
-
+        self.recipe_catogry = None
         self.update_recipe_prefs()
 
     def get_priority(self, recipe : Recipe) -> float:
-        if any(restriction in self.recipies_sorted for restriction in self.restrictions_strict):
+        if any(restriction in recipe for restriction in self.restrictions_strict):
             return -1.0
         
-        priority = 10
+        priority = 10.0
 
-        priority -= sum(restriction in self.recipies_sorted for restriction in self.restrictions_preference)
+        priority -= sum(restriction in recipe for restriction in self.restrictions_preference)
+        priority *= recipe.rating
         
         return priority
 
-    def update_recipe_prefs(self, category=None):
-        
-        self.recipies_sorted = sorted(Database.ALL_RECIPIES, key=lambda recipe : self.get_priority(recipe))
+    def update_recipe_prefs(self, category=Recipe_Categories.ALL):
+        if category == Recipe_Categories.ALL:
+            self.recipies_sorted = sorted(Database.ALL_RECIPIES, key=lambda recipe : self.get_priority(recipe))
+        else:
+            self.recipies_sorted = sorted((recipe for recipe in Database.ALL_RECIPIES if category == recipe.category), key=lambda recipe : self.get_priority(recipe))
 
     def get_recipies():
         """ Get the recipies of highest priority for a given user's preferences
