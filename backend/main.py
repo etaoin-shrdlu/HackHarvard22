909ff2d63cf2f933e135235d85b7d22d41254a5e
etoain-shrdlu <nc8774@gmail.com>
2022-10-15 13:37:54 -0400

    structure of backend

---
+++ backend/main.py
@@ -0,0 +1,38 @@
+recipies = [...]
+
+class Recipe:
+    def __init__(self, **kwargs : ...):
+        
+
+
+class User:
+    def __init__(self, **kwargs : ...):
+        
+        pass
+
+    def get_recipies():
+        """ Get the recipies of highest priority for a given user's preferences
+        """
+        max_recipe_priority = -1
+        for recipe in recipies:
+            recipe_priority = get_priority(recipe)
+            if recipe_priority > max_recipe_priority
+                max_recipe_priority = recipe_priority
+                 
+
+
+        pass
+
+
+
+def get_priority( **kwargs : ... ) -> float:
+    pass
+
+ 
+
+
+
+if __name__ == '__main__':
+    u = User(prefs)
+    
+    get_recipies()
