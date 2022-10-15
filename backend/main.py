8a05c93fbde2959050e11910fa739808e786248e
yashsancheti <97014259+yashsancheti@users.noreply.github.com>
2022-10-15 19:03:06 -0400

    hsdfk

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,14 @@ from tkinter import image_names
 
 from user import User
 
-if __name__ == '__main__':
-    me : User = User(
-        restrictions_strict=['Egg','Dairy'],
-        restrictions_preference=[],
-        skill_level=0
-    )
-    for recipe in me.get_recipies():
-        print(recipe)
+# if __name__ == '__main__':
+#     me : User = User(
+#         restrictions_strict=['Egg','Dairy'],
+#         restrictions_preference=[],
+#         skill_level=0
+#     )
+#     for recipe in me.get_recipies():
+#         print(recipe)
+
+
 
