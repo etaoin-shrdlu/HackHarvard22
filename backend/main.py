232205300351e88a76a5ea8a19f04819081479e9
etoain-shrdlu <nc8774@gmail.com>
2022-10-15 13:56:51 -0400

    ajsdfl

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-import Recipe from recipe
+from user import User
+from recipe import Recipe
 
 recipies = [
 
@@ -6,29 +7,8 @@ recipies = [
 
 
 
-class User:
-    def __init__(self, **kwargs : ...):
-        
-        pass
-
-    def get_recipies():
-        """ Get the recipies of highest priority for a given user's preferences
-        """
-        max_recipe_priority = -1
-        
-        """for recipe in recipies:
-            recipe_priority = get_priority(recipe)
-            if recipe_priority > max_recipe_priority
-                max_recipe_priority = recipe_priority
-        """     
 
 
-class Recipe:
-    def get_priority(self, **kwargs : ... ) -> float:
-        priority = self.ingredients
-        
-        
-        return priority
 
 
 r = Recipe()
