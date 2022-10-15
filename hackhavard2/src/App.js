be9d130b1f12a389d5831509db9fcc801534a2f9
etoain-shrdlu <nc8774@gmail.com>
2022-10-15 12:34:12 -0400

    New files

---
+++ hackhavard2/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import React from 'react';
+import Header from './Header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Questions from './Questions';
+import TinderCards from './TinderCards';
+import ErrorBoundary from './ErrorBoundary';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAJI5DaQrQCALhpCjhJ0qQTwE8SHtdBde0",
+  authDomain: "hack22-recipie-magic.firebaseapp.com",
+  projectId: "hack22-recipie-magic",
+  storageBucket: "hack22-recipie-magic.appspot.com",
+  messagingSenderId: "979812257518",
+  appId: "1:979812257518:web:246bdd86cef0ea532badff",
+  measurementId: "G-1LRBGG2DWB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/question" element={<Questions />} />
+          <Route path="/" element={
+            <ErrorBoundary>
+              <TinderCards />
+            </ErrorBoundary>
+          } />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
