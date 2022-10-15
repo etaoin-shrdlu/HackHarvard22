13ff4467a331ab94b634c26808d2aa8065b43639
etoain-shrdlu <nc8774@gmail.com>
2022-10-15 12:10:23 -0400

    Firebase now functional

--- hackharvardreact/src/App.js
+++ hackharvardreact/src/App.js
@@ -1,6 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAJI5DaQrQCALhpCjhJ0qQTwE8SHtdBde0",
+  authDomain: "hack22-recipie-magic.firebaseapp.com",
+  projectId: "hack22-recipie-magic",
+  storageBucket: "hack22-recipie-magic.appspot.com",
+  messagingSenderId: "979812257518",
+  appId: "1:979812257518:web:246bdd86cef0ea532badff",
+  measurementId: "G-1LRBGG2DWB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
