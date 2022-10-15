a1bf5b3971e156fd12a27ebaaa0eeab809273922
etoain-shrdlu <nc8774@gmail.com>
2022-10-15 13:59:35 -0400

    Finishing measurments architecture

--- backend/measurement.py
+++ backend/measurement.py
@@ -6,4 +6,14 @@ class Units(Enum):
     ...
 
 class Measurement:
-    def __init__(self, num : float, units : )
+    UNIT_CONVERSIONS = [
+        ...
+    ]
+    def __init__(self, num : float, units : Units)
+        self.n = num
+        self.units = units
+
+    def get_amount(self):
+        """ Return the amount in an arbitrary unit for comparisons
+        """
+        return self.n * self.UNIT_CONVERSIONS[self.units]
