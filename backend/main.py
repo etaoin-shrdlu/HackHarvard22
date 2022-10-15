5a3c9823f49cf33bc4625beb0bc21b55bfd0cd59
etoain-shrdlu <nc8774@gmail.com>
2022-10-15 15:09:01 -0400

    Writing parser for csv file of recipies

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,20 @@
+import csv
+
 from user import User
 from recipe import Recipe
 
 class Database:
-    ALL_RECIPIES = [
-        Recipe()
-    ]
+    def recipe_generator():
+        recipies_file = csv.reader('recipies.csv')
+        next(recipies_file) # Skip the first line
+        for recipe in recipies_file:
+            yield Recipe(
+                name=recipe[0],
+                
+            )
+            
+    ALL_RECIPIES = [recipe for recipe in recipe_generator]
+        
 
 
 
@@ -12,5 +22,7 @@ class Database:
 
 
 if __name__ == '__main__':
-    r = Recipe()
-    u = User()
+    u = User(
+
+    )
+
