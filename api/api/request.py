950f3d42f736ee68a48753c095556e30208dae24
yashsancheti <97014259+yashsancheti@users.noreply.github.com>
2022-10-16 01:49:47 -0400

    bugs

--- api/api/request.py
+++ api/api/request.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 import sys
-sys.path.append("/Users/yashsancheti/Yash/HackHarvard22/backend")
-import main
+from backend import main
 
 def start(request):
-    return render(request, 'src/LandingPage/LandingPage.js')
+    return render(request, 'hackhavard2/src/LandingPage/LandingPage.js')
 
 def output(request):
-    return render(request, 'src/Recepies/recipes.js', {'data': main.handleRequest(request = request)})
+    return render(request, 'hackhavard2/src/Recepies/recipes.js', {'data': main.handleRequest(request = request)})
--- api/api/settings.py
+++ api/api/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./hackhavard2'],
+        'DIRS': ['hackhavard2'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
