be001bbe8a8694205b572472a6e2bc289c199f7d
etoain-shrdlu <nc8774@gmail.com>
2022-10-15 22:27:28 -0400

    Important bugfix

--- hackhavard2/src/App/App.js
+++ hackhavard2/src/App/App.js
@@ -7,10 +7,11 @@ import Questions from '../Questions/Questions';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import Entrees from '../Recepies/Recepies';
-import Desserts from '../Dessert/Dessert';
+//import Entrees from '../Recepies/Recepies';
+//import Desserts from '../Dessert/Dessert';
 import LandingPage from '../LandingPage/LandingPage';
-import Appetizers from '../Appetizers/Appetizers';
+//import Appetizers from '../Appetizers/Appetizers';
+import Recepies from '../Recepies/Recepies';
 import AllType from '../AllType/AllType';
 import LoginForm from '../Login/LoginForm';
 
