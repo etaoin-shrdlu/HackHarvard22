6daa57a5c96183f76e0b9993ad3a57dbc83c26a8
etoain-shrdlu <nc8774@gmail.com>
2022-10-16 03:00:08 -0400

    In progress

--- api/hackhavard2/src/LandingPage/LandingPage.js
+++ api/hackhavard2/src/LandingPage/LandingPage.js
@@ -3,9 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import salad from '../salad2.png';
 import './LandingPage.css';
 
-const BACKEND_URL = 'http://127.0.0.1:8000/'
+const BACKEND_URL = 'http://127.0.0.1:8000/';
 
 function makeSearch(country, data) {
+    window.location.pathname = 'recepies?cuisine=' + country + '&categories=' + data;
     let url_data = fetch(BACKEND_URL, {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, cors, *same-origin
@@ -21,14 +22,15 @@ function makeSearch(country, data) {
             'cuisine': country,
             'data': data
         }), // body data type must match "Content-Type" header
-    })
-    this.props.history.push('/recepies/?' + url_data)
+    });
+    
 }
 
 function LandingPage() {
     let countries = [{ label: 'Indian', value: 'indian' }, { label: 'America', value: 'america' }, { label: 'Vietnamese', value: 'vietnamese' }, { label: 'Mexican', value: 'mexican' }];
     let [country, setCountry] = useState("Select cuisine");
     const [data, setData] = useState([]);
+    const [selected_category, setCategory] = useState("");
 
     let addOrRemove = (e) => {
         setCountry(e.target.value);
@@ -46,31 +48,35 @@ function LandingPage() {
                 <Card
                     img={salad}
                     title='Appetizers'
-                    description='Salads are a great way to get your daily dose of vegetables. They are also a great way to get your daily dose of vegetables.'
+                    onclick={setMealCategory('side')}
+                    description='Appetizers are a great way to get your daily dose of vegetables. They are also a great way to get your daily dose of vegetables.'
+
                 />
 
                 <Card
                     img={salad}
                     title='Entrees'
-                    description='Entrees are a great way to get your daily dose of vegetables. They are also a great way to get your daily dose of vegetables.'
+                    onclick={setMealCategory('entree')}
+                    description='Entrees are the heart of a meal, and the most important bit of food! '
                 />
 
                 <Card
                     img={salad}
                     title='Desserts'
-                    description='Desserts are a great way to get your daily dose of vegetables. They are also a great way to get your daily dose of vegetables.'
+                    onclick={setMealCategory('dessert')}
+                    description='Desserts -- for when you want to enjoy life.'
                 />
             </div>
             <div className='crusine__selection'>
                 <select onChange={addOrRemove}>
-                    <option value="Select cruisne">Select cruisne</option>
+                    <option value="Select cruisne">Select cuisine</option>
                     {
                         countries.map((country) => <option key={country.label} value={country.value}>{country.label}</option>)
                     }
                 </select>
             </div>
-            <div className='search__btn'>
-                <button onClick={() => makeSearch(country, data)}>Search</button>
+            <div className='search__btn__container'>
+                <button className='search__btn' onClick={() => makeSearch(country, data)}>Search</button>
             </div>
         </>
     );
@@ -86,20 +92,16 @@ function Card(props) {
         }
     };
 
-    const buttonSwitch = () => {
-        
-    }
-
     return (
         <>
-            <div className='overall__card' onclick={buttonSwitch}>
+            <div className='overall__card' onclick={setCategory}>
                 <div className='card__unclicked'>
                     <div className='card__body'>
                         <img className='card__image' src={props.img} />
                         <h2 className='card__title'>{props.title}</h2>
                         <p className='card__description'>{props.description}</p>
                     </div>
-                    <div className='checkbox__container'>
+                    <div style="display:none" className='checkbox__container'>
                         <input
                             className='card__checkbox'
                             type="checkbox"
